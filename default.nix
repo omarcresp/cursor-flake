fed7bad1e68161f77e114fa13a32f90391692fb6
Omar Crespo <crespomerchano@gmail.coml>
2025-10-05 11:35:41 -0500

    update to 1.7.33

--- default.nix
+++ default.nix
@@ -2,13 +2,14 @@
 
 let
   pname = "cursor";
-  version = "0.50.3";
+  version = "1.7.33";
+  downloadUrl = "https://downloads.cursor.com/production/a84f941711ad680a635c8a3456002833186c484f/linux/x64/Cursor-1.7.33-x86_64.AppImage";
 
   inherit (pkgs.stdenvNoCC) hostPlatform stdenvNoCC;
 
   source = pkgs.fetchurl {
-    url = "https://downloads.cursor.com/production/7ae22cf8cd5af9e08b62585dd03d10f5f610acf9/linux/x64/Cursor-0.50.3-x86_64.AppImage";
-    hash = "sha256-YAQrjNqjORgY9H8ESWYfQO+eH0cvyX56edi5SHMVcXw=";
+    url = downloadUrl;
+    hash = "sha256-bXT/NVqcyR+RrqZdd0TbtcsyLjGb8Wv5S5On9JLElG4=";
   };
 
   appimageContents = appimageTools.extractType2 {
@@ -26,7 +27,8 @@ pkgs.stdenvNoCC.mkDerivation {
   inherit pname version;
   src = if hostPlatform.isLinux then wrappedAppimage else source;
 
-  nativeBuildInputs = pkgs.lib.optionals hostPlatform.isLinux [ pkgs.makeWrapper ]
+  nativeBuildInputs =
+    pkgs.lib.optionals hostPlatform.isLinux [ pkgs.makeWrapper ]
     ++ pkgs.lib.optionals hostPlatform.isDarwin [ pkgs.undmg ];
 
   sourceRoot = pkgs.lib.optionalString hostPlatform.isDarwin ".";
@@ -38,29 +40,34 @@ pkgs.stdenvNoCC.mkDerivation {
     runHook preInstall
     mkdir -p $out/
 
-    ${if hostPlatform.isLinux then ''
-      cp -r bin $out/bin
-      mkdir -p $out/share/cursor
-      cp -a ${appimageContents}/usr/share/cursor/locales $out/share/cursor
-      cp -a ${appimageContents}/usr/share/cursor/resources $out/share/cursor
-      cp -a ${appimageContents}/usr/share/icons $out/share/
-      install -Dm 644 ${appimageContents}/cursor.desktop -t $out/share/applications/
+    ${
+      if hostPlatform.isLinux then
+        ''
+          cp -r bin $out/bin
+          mkdir -p $out/share/cursor
+          cp -a ${appimageContents}/usr/share/cursor/locales $out/share/cursor
+          cp -a ${appimageContents}/usr/share/cursor/resources $out/share/cursor
+          cp -a ${appimageContents}/usr/share/icons $out/share/
+          install -Dm 644 ${appimageContents}/cursor.desktop -t $out/share/applications/
 
-      wrapProgram $out/bin/cursor \
-        --add-flags "\''${NIXOS_OZONE_WL:+\''${WAYLAND_DISPLAY:+--ozone-platform-hint=auto --enable-features=WaylandWindowDecorations --enable-wayland-ime=true}} --no-update"
-    '' else ''
-      APP_DIR="$out/Applications"
-      CURSOR_APP="$APP_DIR/Cursor.app"
-      mkdir -p "$APP_DIR"
-      cp -Rp Cursor.app "$APP_DIR"
-      mkdir -p "$out/bin"
-      cat << EOF > "$out/bin/cursor"
+          wrapProgram $out/bin/cursor \
+            --add-flags "\''${NIXOS_OZONE_WL:+\''${WAYLAND_DISPLAY:+--ozone-platform-hint=auto --enable-features=WaylandWindowDecorations --enable-wayland-ime=true}} --no-update"
+        ''
+      else
+        ''
+          APP_DIR="$out/Applications"
+          CURSOR_APP="$APP_DIR/Cursor.app"
+          mkdir -p "$APP_DIR"
+          cp -Rp Cursor.app "$APP_DIR"
+          mkdir -p "$out/bin"
+          cat << EOF > "$out/bin/cursor"
 
-      #!${stdenvNoCC.shell}
-      open -na "$CURSOR_APP" --args "\$@"
-      EOF
-      chmod +x "$out/bin/cursor"
-    ''}
+          #!${stdenvNoCC.shell}
+          open -na "$CURSOR_APP" --args "\$@"
+          EOF
+          chmod +x "$out/bin/cursor"
+        ''
+    }
 
     runHook postInstall
   '';
